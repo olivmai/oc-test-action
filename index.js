72f5b6e5421bedc1dd5f1febbca663c9160aaadb
Olivier Mairet <oliviermairet@gmail.com>
2021-04-21 16:51:48 +0200

    trying to read xml file from action

--- index.js
+++ index.js
@@ -5,24 +5,31 @@ const convert = require('xml-js');
 const fs = require('fs');
 
 try {
+  // this is the default action code from the tutorial
+  // I keep it for now, as we are sure it works and runs in the CI
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
 
+  ///////////////////////////////////////////
+  // BELOW IS THE CODE FOR OUR REAL ACTION //
+  ///////////////////////////////////////////
+
+  // @actions/glob allow to access files in the current job workplace. See: https://github.com/actions/toolkit/tree/main/packages/glob
   const patterns = ['coverage.xml']
   const globber = glob.create(patterns)
   const file = globber.glob()
-  // try some code for real action
+  
+  // this shoul allow us to get file content
   const xmlContent = fs.readFileSync(file, { encoding: 'utf8' });
 
+  // xml-js extension should ease the process to convert xml content to JSObject or json. See: https://www.npmjs.com/package/xml-js
   var options = {ignoreComment: true, alwaysChildren: true};
   var result = convert.xml2js(xml, options);
 
+  // this result should be a JS object containing coverage infos
   console.log(result);
 
 } catch (error) {
